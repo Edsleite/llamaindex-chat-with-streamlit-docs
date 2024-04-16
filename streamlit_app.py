7954bb36e3184f44ae1ebb38aac331158cb59e61
Marcos Romero <marcos.romero@aluno.ufabc.edu.br>
2024-04-16 17:25:51 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def load_data():
         # llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert o$
         # index = VectorStoreIndex.from_documents(docs)
         embed_model = CohereEmbedding(cohere_api_key=st.secrets.cohere_key,model_name="embed-multilingual-v3.0",input_type="search_query")
-        service_context = ServiceContext.from_defaults(llm=Cohere(api_key=st.secrets.cohere_key,model="commands", temperature=0.5, system_prompt="You are an expert on Core Network Telecomunnications and your job is to answer technical questions. Assume that all questions are related to Core Network equipments. Keep your answers technical and based on facts – do not hallucinate features."),
+        service_context = ServiceContext.from_defaults(llm=Cohere(api_key=st.secrets.cohere_key,model="command", temperature=0.5, system_prompt="You are an expert on Core Network Telecomunnications and your job is to answer technical questions. Assume that all questions are related to Core Network equipments. Keep your answers technical and based on facts – do not hallucinate features."),
                                                     embed_model=embed_model)
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
