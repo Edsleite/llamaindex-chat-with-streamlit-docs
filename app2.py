21efe9473cc9ae098e6016c62a0e6dcdb634cfac
Edsleite <39807952+Edsleite@users.noreply.github.com>
2024-04-25 15:01:19 -0300

    Create app2.py
    same code but adapted to ollama, llama3 and huggingface as embeddings

---
+++ app2.py
@@ -0,0 +1,56 @@
+# chat by romer/eddie
+import streamlit as st
+from llama_index.core import Settings
+from llama_index.llms.ollama import ollama
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+try:
+  from llama_index import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+except ImportError:
+  from llama_index.core import VectorStoreIndex, ServiceContext, Document, SimpleDirectoryReader
+
+st.set_page_config(page_title="Chat with the DB Knowledge Base, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+#cohere.api_key = st.secrets.cohere_key
+st.title("Chat with the DB Knowledge Base, powered by LlamaIndex 💬🦙")
+#st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+         
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about Database and Provisioning Documentation!"}
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the docs – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        # llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert o$
+        # index = VectorStoreIndex.from_documents(docs)
+        llm=ollama(model="llama3", request_timeout=300.0)
+        embed_model = HuggingFaceEmbedding( model_name="BAAI/bge-large-en-v1.5", trust_remote_code=True)
+
+        Settings.embed_model = embed_model
+        index = VectorStoreIndex.from_documents(docs, show_progress=True)
+        Settings.llm = llm
+        
+        return index
+
+index = load_data()
+
+if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
